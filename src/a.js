a75c5835541fd1712f189a1a24ec4993eded13a8
Vyacheslav Zaytcev <vyatcheslav.lacki@gmail.com>
2018-10-06 16:39:33 +0700

    Removed Object.is (see GCC src/com/google/javascript/jscomp/js/es6/object/is.js)

--- src/a.js
+++ src/a.js
@@ -984,28 +984,6 @@ var __GCC__LEGACY_BROWSERS_SUPPORT__OPERA_LT_12_10__ = true;
 
 				return descriptors;
 			}
-
-			,
-			/**
-			 * Object.is
-			 * */
-			"is": function (x, y) {
-				if (x === y) {
-					// 0 === -0, but they are not identical
-					if (x === 0) {
-						return 1 / x === 1 / y;
-					}
-					else {
-						return true;
-					}
-				}
-				// NaN !== NaN, but they are identical.
-				// NaNs are the only non-reflexive value, i.e., if x !== x,
-				// then x is a NaN.
-				// isNaN is broken: it converts its argument to number, so
-				// isNaN("foo") => true
-				return x !== x && y !== y;
-			}
 		});
 
 	}//if( __GCC__ECMA_SCRIPT6__ )
